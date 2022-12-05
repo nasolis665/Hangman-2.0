efd8edca28765f78a39296668d3cd88a30942310
Nathan Solis <113551835+nasolis665@users.noreply.github.com>
2022-12-05 21:22:32 +0000

    underscores

--- game.py
+++ game.py
@@ -1,23 +1,33 @@
 from random import choice
 
 def game_command():
-  tries = 6
-  wordlist=('hot', 'got', 'bought', 'fought', 'lot', 'cot')
-  word=choice(wordlist)
+  num_lives = 6
+  word_list=('hot', 'got', 'bought', 'fought', 'lot', 'cot')
+  word=choice(word_list)
   word_letters=list(word)
   print(word_letters) ##uncomment if you wanna cheat##
   letters_found=[]
   disp_let=[]
-  while tries != 0:
-    letter=input("what letter do you want to guess?")
+  length=len(word_letters)
+  name=input("What is your name? ")
+  print(f"Hello {name} we are playing a game of hang man I'm\n thinking of a {length} letter word and you gotta guess it")
+  undscore=[]
+  dash='_'
+  for i in range(len(word)):
+    undscore.append(dash)
+  while num_lives != 0:
+    letter=input("what letter do you want to guess? ")
     letter=letter.lower()
-    if letter in disp_let:
+    if (letter in disp_let) and (letter not in word_letters):
       print("You already found this letter!")
-    elif letter in word_letters:  
+    elif letter in word_letters:
       print("You got a letter!")
-      index=word_letters.index(letter)
-      disp_let += word_letters[index]
-      print(f"You have these letter's found: {disp_let}")     
+      i=word_letters.index(letter)
+      disp_let += word_letters[i]
+      undscore=undscore[:i]+[letter]+undscore[i+1:]
+      if letter in word_letters >1:
+        pass
+      print(f"You have these letter's found: {undscore}")     
       print("letter's wont be in order")
       letters_found.append(letter)
       word_letters.remove(letter)
@@ -27,8 +37,8 @@ def game_command():
         break
     elif letter not in word_letters:
       print("You did not get a letter")
-      tries -= 1
-      print(f"You have {tries} lives")
-  if tries == 0:
+      num_lives -= 1
+      print(f"You have {num_lives} lives")
+  if num_lives == 0:
     print("You Lose")
-    print(f"you found these letters: {letters_found}, the word was   {word}")
+    print(f"you found these letters: {letters_found}, the word was {word}")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import choice
 from game import game_command
 from variables import var
 
-print("Hello we are playing a game of hang man I'm thinking of a word and you gotta pick it")
+
 
 var()
 game_command()
